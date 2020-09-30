e7b5476b1f2f4adf737a4cade38dd904bf7fefaa
saptakds <saptakds@gmail.com>
2020-09-30 22:15:31 +0530

    Added Singlet Combinations with winning percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -7,5 +7,30 @@ function getFace(){
 		echo "T"
 	fi
 }
-toss=$((1+$RANDOM%2))
-echo `getFace $toss`
+declare -A combi
+flipTimes=21
+#Singlet Combinations
+H=0
+T=0
+for i in `seq 1 $flipTimes`
+do
+	toss=$((1+$RANDOM%2))
+	face=$(getFace $toss)
+	combi[singlet]="${combi[singlet]} $face"
+	if [ $face = "H" ]
+	then
+		((H++))
+	else
+		((T--))
+	fi
+done
+probH=$(echo "scale=2;($H/$flipTimes)*100" | bc )
+probT=$(echo "scale=2;($T/$flipTimes)*100" | bc )
+if [ $H -gt $T ]
+then
+	echo "Winner: H"
+	echo "Percentage: $probH"
+else
+	echo "Winner: T"
+	echo "Percentage: $probT"
+fi
