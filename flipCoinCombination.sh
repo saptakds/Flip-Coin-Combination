fd1ed6257e7b3b2d0a8c67f17f09831425b65c81
saptakds <saptakds@gmail.com>
2020-09-30 23:15:06 +0530

    Added Doublet Combinations with winning percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -10,6 +10,7 @@ function getFace(){
 declare -A combi
 flipTimes=21
 #Singlet Combinations
+echo "Singlet Combinations..."
 H=0
 T=0
 for i in `seq 1 $flipTimes`
@@ -33,4 +34,51 @@ then
 else
 	echo "Winner: T"
 	echo "Percentage: $probT"
+fi
+#Doublet Combinations
+echo "Doublet Combinations..."
+HH=0
+HT=0
+TH=0
+TT=0
+for j in `seq 1 $flipTimes`
+do
+    toss1=$((1+$RANDOM%2))
+	toss2=$((1+$RANDOM%2))
+    face1=$(getFace $toss1)
+	face2=$(getFace $toss2)
+	face="$face1$face2"
+    combi[doublet]="${combi[doublet]} $face"
+    if [ $face = "HH" ]
+    then
+        ((HH++))
+	elif [ $face = "HT" ]
+	then
+		((HT++))
+	elif [ $face = "TH" ]
+	then
+		((TH++))
+    else
+        ((TT--))
+    fi
+done
+probHH=$(echo "scale=2;($HH/$flipTimes)*100" | bc )
+probHT=$(echo "scale=2;($HT/$flipTimes)*100" | bc )
+probTH=$(echo "scale=2;($TH/$flipTimes)*100" | bc )
+probTT=$(echo "scale=2;($TT/$flipTimes)*100" | bc )
+if [ $HH -gt $HT -a $HH -gt $TH -a $HH -gt $TT ]
+then
+    echo "Winner: HH"
+    echo "Percentage: $probHH"
+elif [ $HT -gt $HH -a $HT -gt $TH -a $HT -gt $TT ]
+then
+    echo "Winner: HT"
+    echo "Percentage: $probHT"
+elif [ $TH -gt $HH -a $TH -gt $HT -a $TH -gt $TT ]
+then
+    echo "Winner: TH"
+    echo "Percentage: $probTH"
+else
+    echo "Winner: TT"
+    echo "Percentage: $probTT"
 fi
